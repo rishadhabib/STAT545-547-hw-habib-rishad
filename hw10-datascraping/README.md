3e4ab0807144fbb52155335df2e0db48c8833221
Rishad <rishadhabib@hotmail.com>
2017-12-07 23:36:46 -0800

    sentiment analysis

--- hw10-datascraping/README.md
+++ hw10-datascraping/README.md
@@ -1,1 +1,37 @@
 # hw10 Data Scraping
+
+This repo contains the **10th** and last homework submission for STAT 547
+
+
+## Contents:
+- [HW 10 R script](jcr_scrape.R)
+- [HW 10 markdown file](hw10-datascraping.md)
+- [Readme file](README.md)
+
+
+
+
+## Reporting my Process:
+
+Like previous assignments I first created the homework folder, README file and R script where I created an outline for this homework assignment in my STAT 5457project file. This was a fun assignment and I chose a simple but hopefully useful website to scrape.
+
+For this assignment I wanted to scrape the Journal of Consumer Research website. This is one of the top journals in my field & abstracts are available online. Scraping the data would make it easier for me to search through the abstracts for key terms and topics as well as see which authors published the most in my field. I was trying to make a function that would output all the abstracts and citations for a number of journal articles and managed to get the links all nice and ready using the following:
+
+```{r}
+link <- function(volume = 43, issue = 1){
+
+	# get all combinations of the end of the link
+	link_end <- apply(expand.grid(volume, issue), 1, paste, collapse="/")
+	
+	# glue them all to the first bit
+	lapply(link_end, function(link_end) 
+		glue("https://academic.oup.com/jcr/issue/{link_end}"))
+}	
+
+# test it out
+volume <- 43:44
+issue <- 1:6
+article_link <- as.character(link(volume, issue))
+article_link
+```
+However, the read_html and other functions would not work well with lists and I could not get it into a nice dataframe. The classes on data scraping were very helpful in interpreting the html and writing the code.
--- hw10-datascraping/jcr_scrape.R
+++ hw10-datascraping/jcr_scrape.R
@@ -23,31 +23,31 @@ article_title <- read_html(link)
 # page_title %>% View()
 
 article_title %>%
-  html_nodes(css = ".customLink.item-title a") %>%    # use .title for the class = title
-  html_attr("href")                         # if we want the href part
+	html_nodes(css = ".customLink.item-title a") %>%    # use .title for the class = title
+	html_attr("href")                         # if we want the href part
 
 
 # we will want to read the link to the main article to get the abstract as well as the title, author names, citation, 
 
 jcr <- data_frame(volume = "44",
-                  issue = "3",
-                  
-                  # let's get the title of the article 
-                  title = article_title %>%
-                    html_nodes(css = ".customLink.item-title") %>%    # use .title for the class = title
-                    html_text(trim = TRUE),  
-                  
-                  # and the citation
-                  citation = article_title %>%
-                    html_nodes(css = ".ww-citation-primary") %>%    # use .title for the class = title
-                    html_text(), 
-                  
-                  # and the link to the main article where we can find the abstract
-                  article = article_title %>%
-                    html_nodes(css = ".customLink.item-title a") %>%
-                    html_attr("href"), 
-                  
-                  link = glue("https://academic.oup.com{article}"))
+				  issue = "3",
+				  
+				  # let's get the title of the article 
+				  title = article_title %>%
+				  	html_nodes(css = ".customLink.item-title") %>%    # use .title for the class = title
+				  	html_text(trim = TRUE),  
+				  
+				  # and the citation
+				  citation = article_title %>%
+				  	html_nodes(css = ".ww-citation-primary") %>%    # use .title for the class = title
+				  	html_text(), 
+				  
+				  # and the link to the main article where we can find the abstract
+				  article = article_title %>%
+				  	html_nodes(css = ".customLink.item-title a") %>%
+				  	html_attr("href"), 
+				  
+				  link = glue("https://academic.oup.com{article}"))
 
 jcr$title[1]
 jcr$link
@@ -61,10 +61,10 @@ View(jcr)
 
 # now we write a function to do this
 get_abstract <- function(link){         # this link points to a link that is extracted
-
-    read_html(link) %>%                       # this is the link
-    html_nodes(css = ".abstract") %>%
-    html_text()
+	
+	read_html(link) %>%                       # this is the link
+		html_nodes(css = ".abstract") %>%
+		html_text()
 }
 
 # check to see if function works with a sample article link
@@ -72,21 +72,20 @@ get_abstract("https://academic.oup.com/jcr/article/44/3/477/2939534")
 
 # add to our jcr dataframe
 jcr %<>%
-  mutate(abstract = sapply(jcr$link, get_abstract))
+	mutate(abstract = sapply(jcr$link, get_abstract))
 
 
 # add the author names
 authors <- article_title %>%
-    html_nodes(css = ".al-authors-list") %>%    # use .title for the class = title
-    html_text(trim = TRUE)
+	html_nodes(css = ".al-authors-list") %>%    # use .title for the class = title
+	html_text(trim = TRUE)
 
 authors <- c(authors, rep("NA", nrow(jcr)-length(authors)))
 
 jcr %<>%
-  mutate(authors = authors, abstract = as.character(abstract))
+	mutate(authors = authors, abstract = as.character(abstract))
 
 View(jcr)
 
 write.csv(as.data.frame(jcr), file = "jcr.csv", row.names = FALSE)
-
 
